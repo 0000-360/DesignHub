23729197536a087fc39d42ba90c6d9d0942e63db
Avinash <112330806+Uavina@users.noreply.github.com>
2025-12-30 22:46:48 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-        open: true
-    },
-    build: {
-        outDir: 'dist',
-        sourcemap: false,
-        minify: 'terser',
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-                    'animation-vendor': ['framer-motion', 'gsap', 'react-spring']
-                }
-            }
-        }
-    }
+  plugins: [react()],
 })
